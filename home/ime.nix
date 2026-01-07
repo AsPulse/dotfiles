018585b82f948b600a5116f9216bfe1cd6dd09c2
あすぱる <contact@aspulse.dev>
2026-01-07 10:45:01 +0900

    fix(ime): ensure macSKK loads kana-rule.conf provided by nix

--- home/ime.nix
+++ home/ime.nix
@@ -1,8 +1,23 @@
-{ lib, pkgs, ... }:
 {
-  home.file."Library/Containers/net.mtgto.inputmethod.macSKK/Data/Documents/Settings/kana-rule.conf" =
-    lib.mkIf pkgs.stdenv.isDarwin
-      {
-        source = ../ime/kana-rule.conf;
-      };
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  macSkkSettingsDir = "${config.home.homeDirectory}/Library/Containers/net.mtgto.inputmethod.macSKK/Data/Documents/Settings";
+in
+{
+  # macSKKがnix storeにある設定ファイルを読もうとしてもTCCでブロックされてしまうため、
+  # 直接配置する。
+  home.activation.installMacSkkKanaRule = lib.mkIf pkgs.stdenv.isDarwin (
+    lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      set -eu
+
+      mkdir -p ${lib.escapeShellArg macSkkSettingsDir}
+
+      install -m 0644 ${lib.escapeShellArg (toString ../ime/kana-rule.conf)} \
+        ${lib.escapeShellArg "${macSkkSettingsDir}/kana-rule.conf"}
+    ''
+  );
 }
